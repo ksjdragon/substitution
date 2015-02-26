2a836da33f56ded1aacccda2fdcb3ddc9a836635
unknown <ksjdragon@gmail.com>
2015-02-26 17:00:40 -0500

    initial commit

---
+++ src/bg/replace.js
@@ -0,0 +1,24 @@
+document.documentElement.innerHTML = 
+document.documentElement.innerHTML
+	.replace(/witnesses/g, "these dudes I know")
+	.replace(/allegedly/g, "kinda probably")
+	.replace(/new study/g, "tumblr post")
+	.replace(/rebuild/g, "avenge")
+	.replace(/space/g, "spaaaaaaaaaaaaaace")
+	.replace(/Google Glass/g, "virtual boy")
+	.replace(/car/g, "cat")
+	.replace(/election/g, "eating contest")
+	.replace(/president/g, "garbage man")
+	.replace(/Russian/g, "Ruskie")
+	.replace(/American/g, "Freedom Fighter")
+	.replace(/give/g, "gib")
+	.replace(/gave/g, "gibbed")
+	.replace(/make/g, "mak")
+	.replace(/made/g, "mak-ed")
+	.replace(/mistake/g, "mistak")
+	.replace(/game/g, "gam")
+	.replace(/radioactive/g, "kinda dangerous")
+	.replace(/Democratic Party/g, "Elephants")
+	.replace(/Republican Party/g, "Donkeys")
+	.replace(/Democratic-Donkeys/g, "Elephant-Donkeys");
+	//add all
